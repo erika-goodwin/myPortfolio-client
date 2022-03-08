62de7431f03df38d13dfb44f3fc0e222e61b1b10
erika-goodwin <shellvoerika@gmail.com>
2022-03-07 16:08:05 -0800

    minor fix

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -4,21 +4,23 @@ import styled from "styled-components";
 
 const StyledBurger = styled.button`
   position: absolute;
-  top: 0;
-
-  right: 2rem;
+  top: 1vh;
+  right: 0;
+  right: 2rem; 
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  width: 2rem;
-  height: 2rem;
+  width: auto;
+  height: 4vh;
+  // width: 2rem;
+  // height: 2rem;
   background: transparent;
   border: none;
   cursor: pointer;
   padding: 0;
   z-index: 10;
 
-  margin-top: 0.7rem;
+  // margin-top: 0.7rem;
 
   &:focus {
     outline: none;
